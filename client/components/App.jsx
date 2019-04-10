ace5ed56d88af73b75d56d5c3ba95e99b35741ef
Jonathan Owens"git config --global user.email jo@jondionowens.comgit config --global user.name 'Jonathan Owens <jo@jondionowens.com>
2019-04-09 17:30:33 -0700

    Bot is now responding to text input from the input field. responses appear in client console logs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,18 +16,28 @@ class App extends React.Component {
   componentDidMount () {
     document.getElementById("chat-field").addEventListener('keydown', (e) => {
       if (e.key === "Enter") {
-        this.saveMessage();
+        this.printMessage('user');
       }
   });
   }
 
-  saveMessage() {
-    const messages = this.state.messages;
-    const message = new TextMessage('user', document.getElementById("chat-field").value);
-    console.log(message)
-    messages.push(message);
-    this.setState(messages);
-    document.getElementById("chat-field").value = '';
+  // user inputs a message
+  // it gets added to state and the DOM
+  // it also gets sent to watson
+  // watsons response comes back to the
+
+  printMessage(avatar) {
+    if (avatar === 'user') {
+      const messages = this.state.messages;
+      const message = new TextMessage('user', document.getElementById("chat-field").value);
+      console.log(message)
+      messages.push(message);
+      this.setState(messages);
+      document.getElementById("chat-field").value = '';
+      this.sendMessageToBot(message.body.text);
+    } else {
+
+    }
   };
 
   handleChange(e) {
@@ -37,20 +47,18 @@ class App extends React.Component {
     this.setState(stateChange);
   };
 
-  sendMessage() {
+  sendMessageToBot(message) {
     fetch('/api/bot',{
       method: 'POST',
       body: JSON.stringify({
-        userMessage: this.state.userMessage
+        userMessage: message
       }),
       headers: {"Content-Type": "application/json"}
     })
     .then((res) => {
       return res.json()
     }).then((botResponse) => {
-      const historyUpdate = this.state.messages;
-      historyUpdate.push(botResponse[0]);
-      this.setState({messages: historyUpdate}) 
+      console.log(botResponse)
     });
   }
 
@@ -58,7 +66,7 @@ class App extends React.Component {
     return (
       <div id="wrapper">
         <ChatWindow messages={this.state.messages} />
-        <Textfield saveMessage={this.saveMessage.bind(this)} sendMessage={this.sendMessage.bind(this)}/>
+        <Textfield printMessage={this.printMessage.bind(this)}/>
       </div>
     )
   }
