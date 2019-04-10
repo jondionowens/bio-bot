46e25e7b008141d379b8ef4b122cb211ccc9703c
Jonathan Owens <jonathano@metia.com>
2019-04-10 10:02:25 -0700

    Updated workspace ID to fix non response issue

--- app/bot.js
+++
@@ -1,9 +0,0 @@
-var AssistantV1 = require('watson-developer-cloud/assistant/v1');
-
-var assistant = new AssistantV1({
-  version: 'v1',
-  iam_apikey: 'process.env.WATSON_JONBOT_KEY',
-  url: 'process.env.WATSON_JONBOT_URL'
-});
-
-export default assistant;
