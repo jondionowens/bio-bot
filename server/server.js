24cbba4fc72c82932446065ed334057316a2aa6c
Kashmir Owens <kashmirowens@Kashmirs-MacBook-Air-2.local>
2019-07-06 00:41:48 -0700

    preparing deploy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jonBot = require('./bot/bot.js');
